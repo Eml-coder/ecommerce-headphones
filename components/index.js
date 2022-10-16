477975d9a5b30094068ab8b53c585d259dda0d4b
Eml-coder <labiche6789@gmail.com>
2022-10-16 14:11:46 -0400

    Add structure

---
+++ components/index.js
@@ -0,0 +1,8 @@
+//exporting all the jsx component files here, similar to ejs include boilerplate method.
+export { default as Footer } from './Footer';
+export { default as Layout } from './Layout';
+export { default as Navbar } from './Navbar';
+export { default as Product } from './Product';
+export { default as HeroBanner } from './HeroBanner';
+export { default as FooterBanner } from './FooterBanner';
+export { default as Cart } from './Cart';
