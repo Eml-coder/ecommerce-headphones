ea3a4b0983b99a7c0cceae0b977eecf7bfdd5829
Eml-coder <labiche6789@gmail.com>
2022-10-23 10:35:08 -0400

    completed

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -6,9 +6,13 @@ import { Product } from '../../components';
 import { useStateContext } from '../../context/StateContext';
 
 const ProductDetails = ({ product, products }) => {
-  const { image, name, details, price } = product;
+
+ const {image, name, details, price } = product;
+ //testing 
+ //const {image, name, details, price } = product;
+
   const [index, setIndex] = useState(0);
- const { decQty, incQty, qty, onAdd, setShowCart } = useStateContext();
+  const { decQty, incQty, qty, onAdd, setShowCart } = useStateContext();
  
 
   const handleBuyNow = () => {
@@ -26,7 +30,7 @@ const ProductDetails = ({ product, products }) => {
           </div>
           <div className="small-images-container">
             {image?.map((item, i) => (
-              <img 
+              <img
                 key={i}
                 src={urlFor(item)}
                 className={i === index ? 'small-image selected-image' : 'small-image'}
@@ -55,11 +59,10 @@ const ProductDetails = ({ product, products }) => {
           <p className="price">${price}</p>
           <div className="quantity">
             <h3>Quantity:</h3>
-            {/* syntax error of onclick caused the incremental error*/}
-            <p className="quantity-desc"> 
-               <span className="minus" onClick={decQty}><AiOutlineMinus /></span> 
-              <span className="num">{qty}</span> 
-               <span className="plus" onClick={incQty}><AiOutlinePlus /></span> 
+            <p className="quantity-desc">
+              <span className="minus" onClick={decQty}><AiOutlineMinus /></span>
+              <span className="num">{qty}</span>
+              <span className="plus" onClick={incQty}><AiOutlinePlus /></span>
             </p>
           </div>
           <div className="buttons">
@@ -70,14 +73,14 @@ const ProductDetails = ({ product, products }) => {
       </div>
 
       <div className="maylike-products-wrapper">
-          <h2>View Similar Items</h2>
-          <div className="marquee">
-            <div className="maylike-products-container track">
-              {products.map((item) => (
-                <Product key={item._id} product={item} />
-              ))}
-            </div>
+        <h2>View similar items</h2>
+        <div className="marquee">
+          <div className="maylike-products-container track">
+            {products.map((item) => (
+              <Product key={item._id} product={item} />
+            ))}
           </div>
+        </div>
       </div>
     </div>
   )
@@ -94,7 +97,7 @@ export const getStaticPaths = async () => {
   const products = await client.fetch(query);
 
   const paths = products.map((product) => ({
-    params: { 
+    params: {
       slug: product.slug.current
     }
   }));
@@ -108,7 +111,7 @@ export const getStaticPaths = async () => {
 export const getStaticProps = async ({ params: { slug }}) => {
   const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
   const productsQuery = '*[_type == "product"]'
-  
+
   const product = await client.fetch(query);
   const products = await client.fetch(productsQuery);
 
